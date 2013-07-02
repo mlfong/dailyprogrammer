1495df12c07c55340155e256ddd70b8b7c8c01aa
Man Fong <mlfong@gatech.edu>
2013-07-02 09:35:04 -0400

    improved 131's clojure solution

--- 131/Easy.clj
+++ 131/Easy.clj
@@ -1,7 +1,15 @@
 (defn unit-test [testcase]
-  (let [[num word1 word2] (map read-string (clojure.string/split testcase #" "))]
-    (if 
-      (or
-        (and (= num 0) (= (str word2) (clojure.string/reverse (str word1))))
-          (and (= num 1) (= (str word2) (clojure.string/upper-case (str word1)))))
-            "Good test data" "Mismatch! Bad test data")))
+  (let [[num word1 word2] (clojure.string/split testcase #" ")]
+    (if (or (and (= num "0")
+                 (= word2 (clojure.string/reverse word1)))
+            (and (= num "1")
+                 (= word2 (clojure.string/upper-case word1))))
+        "Good test data"
+        "Mismatch! Bad test data")))
+
+(println(unit-test "0 Car raC"))
+(println(unit-test "0 Alpha AhplA"))
+(println(unit-test "0 Discuss noissucsiD"))
+(println(unit-test "1 Batman BATMAN"))
+(println(unit-test "1 Graph GRAPH"))
+(println(unit-test "1 One one"))
