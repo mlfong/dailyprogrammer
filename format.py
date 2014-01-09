7eeee3f766227146518b56f76f51bc57211322ca
Man Fong <mlfong@gatech.edu>
2014-01-09 00:25:21 -0800

    simplify formatter

--- format.py
+++ format.py
@@ -5,13 +5,11 @@ formats program for /r/dailyprogrammer commenting
 -> adds 4 spaces before each line
 '''
 
-import sys, os
-if __name__=='__main__':
-    if len(sys.argv) != 3: 
-        print 'Usage: format.py [transcript]\nUse - for stdout.'
-        sys.exit(2)
-    if sys.argv[2] != '-': sys.stdout = file(sys.argv[2], 'w')
-    f = open(sys.argv[1])
-    for line in f:
-        print "    "+line,
-    f.close()
+import sys
+if len(sys.argv) != 2:
+  print 'Usage: format.py INPUT'
+  sys.exit(2)
+with open(sys.argv[1]) as f:
+  for line in f:
+    print " "*4+line,
+
